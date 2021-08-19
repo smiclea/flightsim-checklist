3aa21a3d239e2bcca08fde63cc9ce8694cf671de
Sergiu Miclea <smiclea@cloudbasesolutions.com>
2021-08-19 20:01:32 +0300

    Move to tabs

--- client/containers/AirbusChecklistContainer/AirbusChecklistContainer.tsx
+++ client/containers/AirbusChecklistContainer/AirbusChecklistContainer.tsx
@@ -1,23 +1,30 @@
 import {
-  Flex, Heading, Spacer, Text,
+  Button,
+  Checkbox,
+  CircularProgress,
+  CircularProgressLabel,
+  Divider,
+  Flex, Heading, Spacer, Tab, TabList, TabPanel, TabPanels, Tabs, Text,
 } from '@chakra-ui/react'
 import { observer } from 'mobx-react-lite'
 import * as React from 'react'
-import { ChecklistPhase, ChecklistTask } from '../../../models/checklist'
+import { ChecklistPhase, ChecklistTask, checklistTaskIsSeparator } from '../../../models/checklist'
 import useStores from '../../stores/useStores'
-import AirbusChecklistContentModule from './modules/AirbusChecklistContentModule/AirbusChecklistContentModule'
-import AirbusChecklistMenuModule from './modules/AirbusChecklistMenuModule/AirbusChecklistMenuModule'
 
 export default observer(() => {
   const { checklistStore } = useStores()
 
-  const handlePhaseClick = (phase: ChecklistPhase) => {
+  const handlePhaseChange = (phase: ChecklistPhase) => {
     checklistStore.setPhase(phase)
   }
 
   const handleTaskChange = (task: ChecklistTask, isDone: boolean) => {
     checklistStore.setTaskDone(task, isDone)
   }
+
+  const areAllTasksForSelectedPhaseDone = checklistStore.selectedPhase.tasks.filter(t => !checklistTaskIsSeparator(t)).length === checklistStore
+    .selectedPhase.tasks.filter(t => !checklistTaskIsSeparator(t) && t.isDone).length
+  const phaseIndex = checklistStore.airbusChecklist.phases.findIndex(p => p.name === checklistStore.selectedPhase.name)
 
   return (
     <Flex direction="column" p={4}>
@@ -27,6 +34,57 @@ export default observer(() => {
         <Text fontSize="small">by Sergiu Miclea</Text>
       </Flex>
       <Flex>
+        <Tabs
+          orientation="vertical"
+          index={phaseIndex}
+          onChange={idx => { handlePhaseChange(checklistStore.airbusChecklist.phases[idx]) }}
+          isLazy
+        >
+          <TabList minW="290px" position="fixed">
+            {checklistStore.airbusChecklist.phases.map(phase => {
+              const tasksTotal = phase.tasks.filter(t => !checklistTaskIsSeparator(t)).length
+              const tasksDone = phase.tasks.filter(t => !checklistTaskIsSeparator(t) && t.isDone).length
+              const percentage = (tasksDone / tasksTotal) * 100
+              return (
+                <Tab justifyContent="start" key={phase.name}>
+                  {phase.name}
+                  <Spacer />
+                  <CircularProgress ml={4} value={percentage} size="32px" color={percentage === 100 ? 'green' : undefined}>
+                    <CircularProgressLabel>{Math.floor(percentage)}%</CircularProgressLabel>
+                  </CircularProgress>
+                </Tab>
+              )
+            })}
+          </TabList>
+          <TabPanels position="absolute" left="350px" width="auto">
+            <TabPanel pt={0}>
+              <Flex direction="column">
+                <Heading size="md" mb={4}>{checklistStore.selectedPhase.name} Checklist</Heading>
+                <Flex direction="column" mb={8}>
+                  {checklistStore.selectedPhase.tasks.map((task, i) => (
+                    checklistTaskIsSeparator(task) ? <Divider key={i} my={2} /> : (
+                      <Checkbox
+                        key={task.id}
+                        isChecked={task.isDone}
+                        onChange={e => handleTaskChange(task, e.target.checked)}
+                        mb={2}
+                      >{task.name}
+                      </Checkbox>
+                    )
+                  ))}
+                </Flex>
+                {checklistStore.nextPhase ? (
+                  <Button
+                    colorScheme={areAllTasksForSelectedPhaseDone ? 'green' : undefined}
+                  >Next Phase: {checklistStore.nextPhase.name}
+                  </Button>
+                ) : null}
+              </Flex>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Flex>
+      {/* <Flex>
         <Flex pr={8}>
           <AirbusChecklistMenuModule
             checklist={checklistStore.airbusChecklist}
@@ -40,7 +98,7 @@ export default observer(() => {
             onTaskChange={handleTaskChange}
           />
         </Flex>
-      </Flex>
+      </Flex> */}
     </Flex>
   )
 })
